8ac72c60a63b71b9dbbf9caa644292dbdd42e194
wangcheng_hz <wangcheng_hz@126.com>
2018-09-08 13:14:24 -0400

    Involve zmap for scaning validate ip address as first step in python script, it's more faster that use raw http request

--- iprange/iprange.py
+++ iprange/iprange.py
@@ -79,7 +79,7 @@ def parse_one_line_ipaddr(key, s):
         return
     s = "%s %s" % (list[0], list[1])
     score = IP.IP(list[0]).int()
-    write_to_redis_zadd(key, s, score)
+    write_to_redis_zadd("city-%s"%key, s, score)
 
 def parse_ipaddr_range():
     for key in city_list:
@@ -89,7 +89,44 @@ def parse_ipaddr_range():
             parse_one_line_ipaddr(key, s)
 
 
+def isValidIpAddr(str):
+    if (len(str)) < len("0.0.0.0") or (len(str)) > len("255.255.255.255"):
+        return False
+    try:
+        IP.IP(str)
+    except:
+        return False
+    return True
+
+def scan_valid_ipaddr(city=None):
+    scan_port = [80, 81, 8081, 8888]
+    conn = redis.Redis("127.0.0.1", 6379)
+    redis_keys = []
+    if city:
+        redis_keys.append(city)
+    else:
+        redis_keys = conn.keys("city-*")
+        
+    for key in redis_keys:     #scaning city
+        ip_list = conn.zrange(key, 0, -1)
+        for ip in ip_list:     #scaning ip ranges
+            redis_str = '-'.join(ip.split())
+            zmap_str = IP.IP(redis_str).strNormal()
+            for port in scan_port:   #scaning for potential camera port
+                redis_cmd = "zmap -p %d %s 8 -i ens3f0" % (port, zmap_str)
+                logger.info(redis_cmd)
+                __, output = commands.getstatusoutput(redis_cmd)
+                raw_iplist = output.split()
+                for raw_ip in raw_iplist:   #parse valid ip addr
+                    if isValidIpAddr(raw_ip):
+                        store_key = "raw-ip-for-%s" % key
+                        store_str = "%s:%d" % (raw_ip, port)
+                        store_score = IP.IP(raw_ip).int()
+                        conn.zadd(store_key, store_str, store_score)
+
+
 if __name__ == "__main__":
     #download_ipaddr_range()
-    #clean_all_keys()
+    clean_all_keys()
     parse_ipaddr_range()
+    scan_valid_ipaddr()
